038275ce3c9fe9940de1e47358cbf1c875f10a9d
maher <mansourmaher77@gmail.com>
2024-06-02 16:38:59 +0100

    Refactor image domain whitelist in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,11 @@ const nextConfig = {
 
   reactStrictMode: false,
   images: {
-    domains: ["utfs.io", "avatars.githubusercontent.com","lh3.googleusercontent.com"],
+    domains: [
+      "utfs.io",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
   },
   experimental: {
     serverActions: {
